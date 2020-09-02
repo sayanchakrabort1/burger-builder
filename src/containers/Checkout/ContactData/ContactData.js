a7a68cf5d0f4f7eeb105d90c1b9d821308231987
Sayan Chakraborty <sayanchakraborty1996@gmail.com>
2020-09-02 21:15:54 +0530

    Dev-> checkout route completed

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
+import axios from '../../../axios-orders';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 
 class ContactData extends Component {
     state ={
@@ -9,18 +11,59 @@ class ContactData extends Component {
         address: {
             street: '',
             postalCode: ''
-        }
+        },
+        loading: false,
+    }
+
+    orderHandler = (e) => {
+        e.preventDefault();
+        this.setState({
+            loading: true
+        });
+
+        const order = {
+            ingredients: this.props.ingredients,
+            price      : this.props.totalPrice,
+            customer   : {
+                name: 'Sayan',
+                email: 'sayan@kk.com',
+                address: {
+                    country: 'India'
+                },
+                deliveryMethod: 'fastest'
+            }
+        };
+
+        axios.post('/orders.json' , order).then(response => {
+            this.setState({
+                loading: false
+            });
+            this.props.history.push('/');
+        }).catch(
+            error => {
+                this.setState({
+                    loading: true
+                });
+            }
+        );
     }
 
     render () {
+        let form = (
+            <form>
+                <input className={classes.Input}type="text" name="name" placeholder="your name"/>
+                <Button btnType="Success" clicked={this.orderHandler}>Go!</Button>
+            </form>
+        );
+
+        if(this.state.loading){
+            form = <Spinner />
+        }
 
         return (
             <div className={classes.ContactData}>
                 <h4>Enter your contact Data</h4>
-                <form>
-                    <input className={classes.Input}type="text" name="name" placeholder="your name"/>
-                    <Button btnType="Success">Go!</Button>
-                </form>
+                {form}
             </div>
         );
     }
